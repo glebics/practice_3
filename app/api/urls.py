75b43257f982ab64ee10960561575c498a2b84ba
g.rahimzhanov <cubicscience@yandex.ru>
2024-11-16 11:06:05 +0300

    task realized

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,7 +1,32 @@
 from django.urls import path
-from .views import index
+from .views import DogList, DogDetail, BreedList, BreedDetail
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+# Настройка для генерации схемы и документации
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Dog API",
+        default_version='v1',
+        description="API для управления собаками и породами",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
-    path('', index),
+    path('api/dogs/', DogList.as_view(), name='dog-list'),
+    path('api/dogs/<int:id>/', DogDetail.as_view(), name='dog-detail'),
+    path('api/breeds/', BreedList.as_view(), name='breed-list'),
+    path('api/breeds/<int:id>/', BreedDetail.as_view(), name='breed-detail'),
+
+
+    # URL для Swagger и ReDoc
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
 ]
--- app/config/settings.py
+++ app/config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
